d8fd8a003358165f6080fa9752e4feb3ab1ef176
bjung <bikrantjung1214@gmail.com>
2024-02-05 19:31:13 +0545

    Changed title

--- src/config/site.ts
+++ src/config/site.ts
@@ -1,19 +1,18 @@
-import { SiteConfig } from "@/types"
+import { SiteConfig } from '@/types'
 
-import { env } from "@/env.mjs"
+import { env } from '@/env.mjs'
 
 export const siteConfig: SiteConfig = {
-  name: "Next Entree",
-  author: "redpangilinan",
-  description:
-    "Next.js 14+ starter template with app router, shadcn/ui, typesafe env, icons and configs setup.",
-  keywords: ["Next.js", "React", "Tailwind CSS", "Radix UI", "shadcn/ui"],
+  name: 'Shadify',
+  author: 'bikrantjung',
+  description: 'Color variables and tailwind config generator for shadcn',
+  keywords: ['Next.js', 'React', 'Tailwind CSS', 'Radix UI', 'shadcn/ui'],
   url: {
     base: env.NEXT_PUBLIC_APP_URL,
-    author: "https://rdev.pro",
+    author: 'https://rdev.pro',
   },
   links: {
-    github: "https://github.com/redpangilinan/next-entree",
+    github: 'https://github.com/redpangilinan/next-entree',
   },
   ogImage: `${env.NEXT_PUBLIC_APP_URL}/og.jpg`,
 }
